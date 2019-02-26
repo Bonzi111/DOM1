50128618ee1fec3ee6e3500b5aa6e31cfa1ca8af
Shantanu <42547471+Bonzi111@users.noreply.github.com>
2019-02-27 02:09:25 +0530

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,105 @@
+function sorting(i)
+    {
+        switch(i){
+        case 0:
+            let flag=0;
+            if(flag[i]=!0)
+            {
+                for (var i = 0; i < json.length; ++i) 
+                {
+                    for ( var j = i + 1; j <json.length; ++j)
+                    {
+                        if (json[i].firstName > json[j].firstName) 
+                        {
+                            temp =  json[i];
+                            json[i] = json[j];
+                            json[j] = temp;
+                        }
+                    }
+                    console.log(json[i])
+                }
+                flag++;
+            }
+            break;
+        case 1:
+            let flag1=0;
+            if(flag1[i]=!0)
+            {
+                for (var i = 0; i < json.length; ++i) 
+                {
+                    for ( var j = i + 1; j <json.length; ++j)
+                    {
+                        if (json[i].lastName > json[j].lastName) 
+                        {
+                            temp =  json[i];
+                            json[i] = json[j];
+                            json[j] = temp;
+                        }
+                    }
+                    console.log(json[i])
+                }
+                flag1++;            
+            }
+            break;
+        case 2:
+            let flag2=0;
+            if(flag2[i]=!0)
+            {
+                for (var i = 0; i < json.length; ++i) 
+                {
+                    for ( var j = i + 1; j <json.length; ++j)
+                    {
+                        if (json[i].lastName > json[j].lastName) 
+                        {
+                            temp =  json[i];
+                            json[i] = json[j];
+                            json[j] = temp;
+                        }
+                    }
+                    console.log(json[i])
+                }
+                flag2++;            
+            }
+        case 3:
+            let flag3=0;
+            if(flag3[i]=!0)
+            {
+                for (var i = 0; i < json.length; ++i) 
+                {
+                    for ( var j = i + 1; j <json.length; ++j)
+                    {
+                        if (json[i].id > json[j].id) 
+                        {
+                            temp =  json[i];
+                            json[i] = json[j];
+                            json[j] = temp;
+                        }
+                    }
+                    console.log(json[i])
+                }
+                flag3++;
+            }
+        case 4:
+            let flag4=0;
+            if(flag4[i]=!0)
+            {
+                for (var i = 0; i < json.length; ++i) 
+                {
+                    for ( var j = i + 1; j <json.length; ++j)
+                    {
+                        if (json[i].phoneNumber > json[j].phoneNumber) 
+                        {
+                            temp =  json[i];
+                            json[i] = json[j];
+                            json[j] = temp;
+                        }
+                    }
+                    console.log(json[i])
+                }
+                flag4++;
+            }
+        }
+        }
 var json = 
 [
     {
@@ -17,43 +119,87 @@ var json =
         "PhoneNo.": 7055473817,
         "Designation": "Sw Trainee",
         "DateOfBirth": "19-1-96",
-    }
-]
-function  createTable() 
-{      
-    var HeadData=[];
-    for(var index = 0; index < json.length; index++)
+    },
     {
-        for (var key in json[index])
+            "firstName": "Jitendra",
+            "lastName": "sharma",
+            "email": "jai22034@gmail.com",
+            "id": 6181,
+            "phoneNumber": 9569505450 ,
+            "designation": "Sw Trainee",
+            "DOB": "30-07-98",
+        },
         {
-            if (HeadData.indexOf(key) === -1){
-                HeadData.push(key);
+            "firstName": "Shivani",
+            "lastName": "Singh",
+            "email": "shivani.singh@gmail.com",
+            "id": 7823,
+            "phoneNumber": 7827888728,
+            "designation": "Sw Trainee",
+            "DOB": "4-3-97",
+        },
+        {
+            "firstName": "Teena",
+            "lastName": "Khandelwal",
+            "email": "teena.khandelwal@gmail.com",
+            "id": 2311,
+            "phoneNumber":7827378203 ,
+            "designation": "Sw Trainee",
+            "DOB": "5-8-96",
+        },
+        {
+            "firstName": "Lakshay",
+            "lastName": "Kapoor",
+            "email": "lakshay.kapoor@gmail.com",
+            "id": 6543,
+            "phoneNumber": 9182938389,
+            "designation": "Sw Trainee",
+            "DOB": "21-11-95",
+        },
+]
+  function  createTable() {
+    var headerData=[];
+    for(var index = 0; index < json.length; index++){
+        for (var key in json[index]){
+            if (headerData.indexOf(key) === -1){
+                headerData.push(key);
             }
         }
     }
+
+    
+
     var table = document.createElement("table");
+
     var tr = document.createElement("tr");
     table.appendChild(tr)
-    var count=0;
-    for(var index = 0; index < HeadData.length; index++ ){
+    var ar=0;
+
+    for(var index = 0; index < headerData.length; index++ ){
         var th = document.createElement("th");
-        th.innerHTML = HeadData[index];
-        th.setAttribute("onclick","sorting("+count+")" );
-        count+=1;
+        th.innerHTML = headerData[index];
+        th.setAttribute('onclick',"sorting("+ar+")")
+        ar+=1;
         tr.appendChild(th);
     }
-    onclick="myFunction()"
-    for (var index = 0; index < json.length; index++) 
-    {
-        tr = document.createElement("tr");
-        table.appendChild(tr);
-        for (var index1 = 0; index1 < HeadData.length; index1++) 
-        {
-            var tableData = document.createElement("td");
-            tr.appendChild(tableData)
-            tableData.innerHTML = json[index][HeadData[index1]];
-        }
-    }
-    document.body.appendChild(table);
+    
+
+    for (var index = 0; index < json.length; index++) {
+
+tr = document.createElement("tr");
+table.appendChild(tr);
+
+for (var index1 = 0; index1 < headerData.length; index1++) {
+    var tableData = document.createElement("td");
+    tr.appendChild(tableData)
+    tableData.innerHTML = json[index][headerData[index1]];
 }
-createTable();
+
+}
+
+var divContainer = document.getElementById("showData");
+divContainer.innerHTML = "";
+divContainer.appendChild(table);
+
+
+}
